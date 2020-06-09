4db6825e48a5610f4e96bba5eddb3f22dae86b10
oookevin53 <oookevin53@gmail.com>
2020-06-09 17:04:50 -0400

    completed 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,7 +9,29 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
-
+  
+  useEffect(() => {
+    console.log("effect")
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
+  
+  const handleNewContact = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const handleNewNum = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+  
   const addContact = (event) => {
     event.preventDefault()
     
@@ -17,37 +39,19 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
+  
     if (persons.some(person => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-    setPersons(persons.concat(contactInfo))
-    setNewName('')
-    setNewNumber('')
+        axios
+          .post("http://localhost:3001/persons", contactInfo)
+          .then(response => {
+            setPersons(persons.concat(response.data))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
     }
-  }
-
-  const handleNewContact = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNewNum = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleSearch = (event) => {
-    setSearch(event.target.value)
-  }
-
-  useEffect(() => {
-    console.log("effect")
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  }, [])
 
   return (
     <div>
@@ -60,7 +64,7 @@ const App = () => {
         addContact={addContact}
         handleNewContact={handleNewContact}
         handleNewNum={handleNewNum}
-      />
+        />
       <h3>Numbers</h3>
       <List list={persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))} />
     </div>
