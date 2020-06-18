cb440f61aa9c0e6be7aae06b139be8f33943cb91
oookevin53 <oookevin53@gmail.com>
2020-06-18 14:48:05 -0400

    completed 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,6 +27,10 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get("/info", (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people<br/><br/>${Date()}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server runninig on port ${PORT}`)
