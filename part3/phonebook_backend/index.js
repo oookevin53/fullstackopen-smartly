d5b436e23d757279f044ea4a157fa7e6cc2f9010
oookevin53 <oookevin53@gmail.com>
2020-06-18 14:53:07 -0400

    completed 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -23,12 +23,23 @@ app.get("/", (request, response) => {
     response.send("<h1>Hello Phonebook</h1>")
 })
 
+app.get("/info", (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people<br/><br/>${Date()}`)
+})
+
 app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
-app.get("/info", (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people<br/><br/>${Date()}`)
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
